d427efd3fa21dc364c9f136e4cf6c96992aab926
karlahaiat <49701058+karlahaiat@users.noreply.github.com>
2019-08-02 21:39:29 -0400

    Includes description of what each script does

--- startup.py
+++ startup.py
@@ -3,12 +3,16 @@ import threading
 import os
 
 def startprgm(i):
+	#This script turns on led and logs time
     	if (i == 0):
         	os.system("sudo python /home/pi/camtag/ledstart.py")
+	#This script starts the gps
     	elif (i == 1):
         	os.system("sudo python /home/pi/camtag/sensors/gps.py")
+	#This script starts all the i2c sensors
 	elif (i == 2):
 		os.system("sudo python /home/pi/camtag/sensors/read_sensors.py")
+	#This script starts the camera feeds
 	elif (i ==3):
 		os.system("sudo python /home/pi/camtag/sensors/camera.py")
 	else:
